891a1ba1e953fdf6ab2ee3c474f0416b90a609d6
Gary Doublé <garydouble@live.com>
2017-11-19 11:41:29 +0000

    added crude release script

---
+++ release.ps1
@@ -0,0 +1,15 @@
+
+param([string]$versionSuffix="")
+param([string]$apiKey="")
+
+
+rm dist/*
+
+if ($versionSuffix -ne "") {
+  dotnet pack source -c Debug --include-symbols --include-source --version-suffix $versionSuffix -o ../../dist
+}
+else {
+  dotnet pack source -c Release --include-symbols --include-source -o ../../dist
+}
+
+nuget push ".\dist\*.nupkg" -src https://api.nuget.org/v3/index.json -ApiKey $apiKey
